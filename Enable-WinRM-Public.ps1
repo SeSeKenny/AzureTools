0ad53e16b811336a5b6ad9779bd266871dc8cca9
SeSeKenny <skenny@wfap.ca>
2019-11-26 13:54:25 -0700

    Ensure service is started

--- Enable-WinRM-Public.ps1
+++ Enable-WinRM-Public.ps1
@@ -1,3 +1,6 @@
 Get-NetFirewallRule -DisplayGroup "Windows Remote Management" `
   | Where-Object Profile -eq 'Public' `
   | Set-NetFirewallRule -Enabled True -RemoteAddress Any
+
+Set-Service WinRM -StartupType Automatic
+Start-Service WinRM
